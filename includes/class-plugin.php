98c825f6d4fa54032f7110c405578d82ab68a98e
aawazjanata <aawazjanatecha@gmail.com>
2026-08-30 00:47:10 +0530

    Fix PHP parse error in class-plugin.php - restore complete Plugin class structure

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -1,17 +1,75 @@
 <?php
-// ... existing file header ...
-// Replace or update the admin_assets() method with the following:
+/**
+ * Sovexxa Plugin Main Class
+ *
+ * @package Sovexxa
+ */
 
-public function admin_assets() {
-	wp_enqueue_style( 'sovexxa-admin-css', SOVEXXA_PLUGIN_URL . 'admin/css/admin.css', [], '1.0' );
-	wp_enqueue_script( 'papaparse', 'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js', [], '5.3.2', true );
-	wp_enqueue_script( 'sovexxa-admin-js', SOVEXXA_PLUGIN_URL . 'admin/js/admin.js', [ 'jquery', 'papaparse' ], '1.0', true );
+namespace Sovexxa;
 
-	// Enqueue invoices admin script (used on invoices admin view)
-	wp_enqueue_script( 'sovexxa-invoices-admin-js', SOVEXXA_PLUGIN_URL . 'admin/js/invoices-admin.js', [ 'jquery' ], '1.0', true );
+/**
+ * Main Plugin Class
+ */
+class Plugin {
 
-	wp_localize_script( 'sovexxa-admin-js', 'sovexxa_admin', [
-		'ajax_url' => admin_url( 'admin-ajax.php' ),
-		'mapping_nonce' => wp_create_nonce( 'sovexxa_mapping_nonce' ),
-	] );
+	/**
+	 * Initialize the plugin
+	 */
+	public function init() {
+		add_action( 'admin_enqueue_scripts', [ $this, 'admin_assets' ] );
+		add_action( 'admin_menu', [ $this, 'admin_menu' ] );
+	}
+
+	/**
+	 * Enqueue admin assets
+	 */
+	public function admin_assets() {
+		wp_enqueue_style(
+			'sovexxa-admin-css',
+			SOVEXXA_PLUGIN_URL . 'admin/css/admin.css',
+			[],
+			'1.0'
+		);
+
+		wp_enqueue_script(
+			'papaparse',
+			'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js',
+			[],
+			'5.3.2',
+			true
+		);
+
+		wp_enqueue_script(
+			'sovexxa-admin-js',
+			SOVEXXA_PLUGIN_URL . 'admin/js/admin.js',
+			[ 'jquery', 'papaparse' ],
+			'1.0',
+			true
+		);
+
+		wp_enqueue_script(
+			'sovexxa-invoices-admin-js',
+			SOVEXXA_PLUGIN_URL . 'admin/js/invoices-admin.js',
+			[ 'jquery' ],
+			'1.0',
+			true
+		);
+
+		wp_localize_script(
+			'sovexxa-admin-js',
+			'sovexxa_admin',
+			[
+				'ajax_url'      => admin_url( 'admin-ajax.php' ),
+				'mapping_nonce' => wp_create_nonce( 'sovexxa_mapping_nonce' ),
+			]
+		);
+	}
+
+	/**
+	 * Register admin menu pages
+	 */
+	public function admin_menu() {
+		// Add your admin menu code here
+	}
+
 }
